18dff89c0d51f437e07a31015bee9289f49e6e3f
Mads Rasmussen <madshr2005@gmail.com>
2025-10-28 12:20:39 +0100

    feat: Update database settings to use PostgreSQL with environment variables

--- tp_backend/settings.py
+++ tp_backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,10 +75,14 @@ WSGI_APPLICATION = 'tp_backend.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("DB_NAME", "tp"),
+        "USER": os.environ.get("DB_USER", "tp"),
+        "PASSWORD": os.environ.get("DB_PASS", "tp"),
+        "HOST": os.environ.get("DB_SERVICE", "localhost"),
+        "PORT": os.environ.get("DB_PORT", "5432"),
+    },
 }
 
 

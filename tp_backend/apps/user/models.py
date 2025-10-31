22ae093a1c62484b35500a15f2aa22f10380b8ee
Mads Rasmussen <madshr2005@gmail.com>
2025-10-31 09:41:30 +0100

    chore: Comment out unused imports in user model for cleaner code

--- tp_backend/apps/user/models.py
+++ tp_backend/apps/user/models.py
@@ -3,9 +3,9 @@ import uuid
 
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.db.models.signals import pre_save
+""" from django.db.models.signals import pre_save
 from django.dispatch import receiver
-from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.models import Token """
 
 from .constants import Roles
 
